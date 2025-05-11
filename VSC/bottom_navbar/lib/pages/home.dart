bfd56caf97de26d57fa5902287bf49116c97fa28
Dominic Whye <75233984+dombennic@users.noreply.github.com>
2025-05-11 20:25:48 +0800

    Add files via upload

---
+++ VSC/bottom_navbar/lib/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import'reminders.dart';
+import 'gratitude.dart';
+import 'birthdays.dart';
+
+class Home extends StatefulWidget{
+  @override
+  _HomeState createState()=>_HomeState();
+}
+
+class _HomeState extends State<Home>{
+  
+}
